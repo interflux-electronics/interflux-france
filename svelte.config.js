e00e29e6898b820d9c360fbf995306cbea5023f6
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-01-26 22:35:06 +1100

    Configure node-adaptor and vitePreprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,14 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+    kit: {
+        adapter: adapter({
+            envPrefix: 'SK_'
+        })
+    },
+    preprocess: vitePreprocess()
+};
 
 export default config;
