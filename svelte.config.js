a8fe582b62af79ef7670ab28b64c0d878fe92cc2
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-05-29 11:27:03 +1000

    Allow SCSS to import from src/lib/

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,26 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter({
-            envPrefix: 'APP_'
-        })
-    },
-    preprocess: vitePreprocess()
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess({
+    scss: {
+      // Resolve $lib in import paths
+      // @import '$lib/styles/mixins.scss';
+      // @import '$lib/styles/variables.scss';
+      importer: [
+        (url) => {
+          if (url.startsWith('$lib/')) {
+            return {
+              file: path.resolve(__dirname, 'src/lib', url.slice(5))
+            };
+          }
+          return null; // Let SASS handle other imports normally
+        }
+      ]
+    }
+  })
 };
 
 export default config;
